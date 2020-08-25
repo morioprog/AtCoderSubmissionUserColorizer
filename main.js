2e7f17e48ab7b11bfa4562d7966d13419b65947a
morioprog <morio.prog@gmail.com>
2020-08-26 00:41:38 +0900

    refactor

--- main.js
+++ main.js
@@ -10,32 +10,32 @@
 // @require      https://unpkg.com/lscache/lscache.min.js
 // ==/UserScript==
 
-function getcolor(rating) {
+function getColor(rating) {
     if (rating >= 2800) return '#FF0000';
     if (rating >= 2400) return '#FF8000';
     if (rating >= 2000) return '#C0C000';
     if (rating >= 1600) return '#0000FF';
     if (rating >= 1200) return '#00C0C0';
-    if (rating >= 800)  return '#008000';
-    if (rating >= 400)  return '#804000';
-    if (rating >= 0)    return '#808080';
+    if (rating >=  800) return '#008000';
+    if (rating >=  400) return '#804000';
+    if (rating >=    0) return '#808080';
     return '#000000';
 }
 
-function getcolorclass(rating) {
+function getColorClass(rating) {
     if (rating >= 2800) return 'user-red';
     if (rating >= 2400) return 'user-orange';
     if (rating >= 2000) return 'user-yellow';
     if (rating >= 1600) return 'user-blue';
     if (rating >= 1200) return 'user-cyan';
-    if (rating >= 800)  return 'user-green';
-    if (rating >= 400)  return 'user-brown';
-    if (rating >= 0)    return 'user-gray';
+    if (rating >=  800) return 'user-green';
+    if (rating >=  400) return 'user-brown';
+    if (rating >=    0) return 'user-gray';
     return 'user-unrated';
 }
 
-function getachrate(rating) {
-    var base = Math.floor(rating / 400) * 400;
+function getAchRate(rating) {
+    const base = Math.floor(rating / 400) * 400;
     return ((rating - base) / 400) * 100;
 }
 
@@ -44,8 +44,8 @@ function colorize(u, rating) {
     else if (rating >= 3600) $(u).before('<img style="vertical-align: middle;" src="//img.atcoder.jp/assets/icon/crown3600.gif">&nbsp;');
     else if (rating >= 3200) $(u).before('<img style="vertical-align: middle;" src="//img.atcoder.jp/assets/icon/crown3200.gif">&nbsp;');
     else {
-        var color = getcolor(rating);
-        var achrate = getachrate(rating);
+        const color = getColor(rating);
+        const achRate = getAchRate(rating);
         $(u).before(`
             <span style="
                 display: inline-block;
@@ -57,13 +57,13 @@ function colorize(u, rating) {
                 background: -webkit-linear-gradient(
                     bottom,
                     ${color} 0%,
-                    ${color} ${achrate}%,
-                    rgba(255, 255, 255, 0.0) ${achrate}%,
+                    ${color} ${achRate}%,
+                    rgba(255, 255, 255, 0.0) ${achRate}%,
                     rgba(255, 255, 255, 0.0) 100%);
             "></span>
         `);
     }
-    $(u).addClass(getcolorclass(rating));
+    $(u).addClass(getColorClass(rating));
 }
 
 $(function() {
@@ -71,14 +71,14 @@ $(function() {
 
     lscache.flushExpired();
 
-    $('a[href*="/users"]').each(function(i, u) {
+    $('a[href*="/users"]').each(function(_, u) {
         // Skip "My Profile"
         if ($(u).find('span').length) return true;
 
-        var username = $(this).attr('href').slice(7);
-        var lskey = "rating-" + username;
-        var rating = lscache.get(lskey);
-        
+        const userName = $(this).attr('href').slice(7);
+        const lskey = "rating-" + userName;
+        let rating = lscache.get(lskey);
+
         if (rating === null) {
             $.ajax({
                 url: "https://atcoder.jp" + $(this).attr('href') + "/history/json",
@@ -86,14 +86,14 @@ $(function() {
                 dataType: 'json'
             })
             .done(function(data) {
-                var ratedcount = data.length;
-                if (ratedcount == 0) {
+                const ratedCount = data.length;
+                if (ratedCount == 0) {
                     rating = 0;
                 } else {
-                    rating = data[ratedcount - 1]["NewRating"];
+                    rating = data[ratedCount - 1]["NewRating"];
                 }
-                // 12 hours
-                lscache.set(lskey, rating, 12 * 60);
+                // Update every 3 hours
+                lscache.set(lskey, rating, 3 * 60);
             })
             .then(function() {
                 colorize(u, rating);
